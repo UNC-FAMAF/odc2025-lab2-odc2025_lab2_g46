a7bb3653159db9f95206bcb5298d8f22816be851
santiago <g4llard0mas@gmail.com>
2025-06-05 14:04:04 -0300

    Sky,Dirt and Delay

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -12,9 +12,11 @@ main:
 	// x0 contiene la direccion base del framebuffer
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
 	//---------------- CODE HERE ------------------------------------
+	
+	//FONDO 1
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+	movz x10, 0xAD, lsl 16
+	movk x10, 0xD8E6, lsl 00
 
 	mov x2, SCREEN_HEIGH         // Y Size
 loop1:
@@ -26,6 +28,45 @@ loop0:
 	cbnz x1,loop0  // Si no terminó la fila, salto
 	sub x2,x2,1	   // Decrementar contador Y
 	cbnz x2,loop1  // Si no es la última fila, salto
+	
+	// ---------------------------------------------------
+	// DELAY EJERCICIO 7 TP 8
+	movz x7, 0x1, lsl 0 //SI 48 32 16
+	lsl x7,x7, 27
+L1: 	sub x7, x7, 1 // No existe subi asi que modificamos a sub
+	cbnz x7, L1
+
+
+	// ---------------------------------------------------------
+	// FONDO 2
+
+	// Reusamos el registro x10 para cambiarle el color
+	movz x10, 0x55, lsl 16
+	movk x10, 0x6B2F, lsl 00
+	mov x0,x20
+
+	// Creamos Direccion
+	// EJE Y = 240 , x=640
+	
+	mov x3, 240
+	mov x4, SCREEN_WIDTH
+	
+	mul x3,x3,x4 //total de pixeles a pintar
+	lsl x3,x3,2 //Creacion Direccion
+	add x0,x0,x3 //Guardado en x0 tiene la direccion de donde comenzar
+	
+	mov x1,240
+	mul x1,x1,x4 //Creacion del contador i x1
+	
+	//Aca modifique mi loop del anterior que tenia para que funcione con cbnz
+	//Como se muestra es un solo loop directamente facil para pintar cosas de un tiron
+tierra_loop:
+	stur w11,[x0] //Cargamos color
+	add x0,x0,4 //Avanzamos en la direccion 4 bytes 1 pos
+	sub x1,x1,1 //Restamos contador
+	cbnz x1,tierra_loop // Si x1 es distinto de 0 vuelve
+
+
 
 	// Ejemplo de uso de gpios
 	mov x9, GPIO_BASE
@@ -45,6 +86,7 @@ loop0:
 	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
 	// efectivamente, su valor representará si GPIO 2 está activo
 	lsr w11, w11, 1
+	
 
 	//---------------------------------------------------------------
 	// Infinite Loop
